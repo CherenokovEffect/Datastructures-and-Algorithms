b3fb2e5bbaa6e0857c542730a56f43eb0d080a7e
Maddy <142102472+PriestCoder@users.noreply.github.com>
2026-02-04 17:51:41 +0530

    DFS and BFS

--- Graphs/Traversal/BFS.cpp
+++ Graphs/Traversal/BFS.cpp
@@ -7,12 +7,21 @@ void AddEdge(vector<int> adj[] , int a , int b){
         adj[b].EB(a) ;
 }
 
-void DFS(int s , vector<int> adj[],vector<bool> &visited){
-    if(visited[s]) return ;
+void BFS(int s , vector<int> adj[],vector<bool> &visited){
+    queue<int> q ;
     visited[s] = true ;
-    cout << s << " " ;
-    for(auto it : adj[s]){
-        DFS( it , adj , visited) ;
+
+    q.push(s) ;
+    while(!q.empty()){
+        int u = q.front() ;
+        q.pop();
+        cout << u << " " ;
+        for(auto it : adj[u]){
+            if(visited[it]==false){
+                visited[it] = true ;
+                q.push(it) ;
+            }
+        }
     }
 }
 
@@ -30,5 +39,5 @@ int main(){
       AddEdge(adj,a,b) ;
     }
     int source = 1 ;
-    DFS(source,adj,visited) ;
+    BFS(source,adj,visited) ;
 }
